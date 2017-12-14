8c719783bf415512860c1dfc83e96a9aedcaed8e
Anastasia Kazakova <a.kazakova@zbw.eu>
2017-12-14 15:33:43 +0100

    axios added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    
+
     <router-view/>
   </div>
 </template>
@@ -17,7 +17,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #bb467a;
   margin-top: 60px;
 }
 </style>
