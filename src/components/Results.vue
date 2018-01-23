011941ae150cec2b963c9a23e9cca1e4c919dd08
Nelson Tavares de Sousa <ntd@informatik.uni-kiel.de>
2018-01-23 13:35:55 +0100

    SAI-277 added header and an alert for empty results

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,14 +1,13 @@
 <template>
 <div class="results">
-  <br>
   <search-mask :query-value="$route.query.q"></search-mask>
-
   <b-container>
     <b-row>
       <!-- <b-col cols="3"><search-facetes/></b-col> -->
       <b-col cols="12">
+        <h6 v-if="totalFoundDocs > 0" class="results-annotation">{{ totalFoundDocs }} results found</h6>
         <search-result-entry v-for="result in results" :result="result" :key="result._id"></search-result-entry>
-        <br>
+        <b-alert class="nothing-found-alert" :show="totalFoundDocs == 0">Nothing to see here</b-alert>
       </b-col>
     </b-row>
   </b-container>
@@ -51,12 +50,10 @@ export default {
     search() {
       const self = this
       self.results = []
-      console.log('/api/search?q='.concat(encodeURIComponent(this.$route.query.q)).concat("&from=").concat(this.currentPage * this.numDocsPerPage - this.numDocsPerPage).concat("&size=").concat(this.numDocsPerPage))
       axios.get('/api/search?q='.concat(encodeURIComponent(this.$route.query.q)).concat("&from=").concat(this.currentPage * this.numDocsPerPage - this.numDocsPerPage).concat("&size=").concat(this.numDocsPerPage))
         .then(function(response) {
           self.results = response.data.hits.hits;
           self.totalFoundDocs = response.data.hits.total;
-          console.log(self.totalFoundDocs)
         })
         .catch(function(error) {
           self.errMsg = error.response;
@@ -80,5 +77,17 @@ export default {
 .mask {
   margin-top: 2rem;
   margin-bottom: 2rem;
+}
+.results {
+  margin-top: 1rem;
+}
+.nothing-found-alert {
+  margin-top: 1rem;
+}
+.pagination {
+  margin-top: 1rem;
+}
+.results-annotation {
+  margin-top: 1rem;
 }
 </style>
