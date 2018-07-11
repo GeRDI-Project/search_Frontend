004b625497bc318577a036a2d27213e84fedabab
Anastasia Kazakova <a.kazakova@zbw.eu>
2018-07-11 09:40:08 +0200

    some preparations

--- src/components/BookmarkList.vue
+++ src/components/BookmarkList.vue
@@ -1,23 +1,21 @@
 <template>
+
 <div>
-   <br>
-<b-card class="mb-3" v-for="bookmark in bookmarks"  :key="bookmark._id">
-   <h5><a :href='filterForViewURI(bookmark[0]._source.webLinks)'>{{ bookmark[0]._source.titles[0].value }}</a></h5>
-   <h6>{{bookmark[0]._source.publisher}}</h6>
-
-        <p>
-         {{bookmark[0]._source.publicationYear}}
-         <br>
-        {{bookmark[0]._source.descriptions[0].value}}
-
-        </p>
-          <b-button-group>
-            <b-button disabled variant="link">More information</b-button>
-            <b-button disabled variant="link">Remove</b-button>
-        </b-button-group>
-    </b-card>
+  <b-card class="mb-3" v-for="bookmark in bookmarks" :key="bookmark._id">
+    
+    <h5>
+      <a :href='filterForViewURI(bookmark[0]._source.webLinks)'>{{ bookmark[0]._source.titles[0].value }}</a>
+    </h5>
+    <h6>{{bookmark[0]._id}}</h6>
+    <h6>{{bookmark[0]._source.publisher}}</h6>
+    {{bookmark[0]._source.publicationYear}}
+    <br>
+    <b-button-group>
+      <b-button disabled variant="link">More information</b-button>
+      <b-button disabled variant="link">Remove</b-button>
+    </b-button-group>
+  </b-card>
 </div>
-
 </template>
 
 <script>
@@ -26,11 +24,12 @@
 import usercookie from '../util/usercookie.js'
 import axios from 'axios'
 export default {
-  name: 'bookmark-list-entry',
+  name: 'bookmark-list',
   props: ['collections'],
   data() {
     return {
-      bookmarks: []
+      bookmarks: [],
+      filteredBookmarks: []
     }
   },
   created() {
@@ -63,7 +62,8 @@ export default {
       let limit = 850
       if (result.length > limit) result = result.substr(0,limit) + ' [...]'
       return result
-    } 
+    },
+    
   }
 }
 </script>
--- src/components/Collections.vue
+++ src/components/Collections.vue
@@ -1,24 +1,22 @@
 <template>
 <div>
 <br>
-  <b-tabs>
-  <b-tab title="Collections">
-    
-    <br>
-    <b-list-group v-for="collection in collections" :key="collection.id">
-  <b-list-group-item >{{collection.name}} <br> {{collection._id}}
-  <br>
-  <b-btn class="mt-3" variant="outline-success" block @click="">Show bookmarks</b-btn>
-  </b-list-group-item>
-  
-  
-</b-list-group>
-  </b-tab>
-  <b-tab title="Bookmarks" >
-    <br><bookmark-list v-if="collections.length > 0" :collections="collections"></bookmark-list>
-  </b-tab>
-</b-tabs>
-  
+<b-card class="m-2" v-for="collection in collections" :key="collection.id">
+      <h3>{{collection.name}}</h3>
+      <h5>{{collection._id}}</h5>
+      <br>
+      <b-btn v-b-toggle.collapse1_inner size="sm" v-on:click="showBookmarks" variant="link" class="text-uppercase">Show data sets</b-btn>
+      <b-collapse id=collapse1_inner class="mt-2">
+        <b-card class="m-2" v-for="bookmark in bookmarksForCollection" :key="bookmark.id">   
+          Here should be shown a card with data set
+          <br>
+          <b-button-group>
+            <b-button disabled variant="link">More information</b-button>
+            <b-button disabled variant="link">Remove</b-button>
+          </b-button-group>
+        </b-card>
+      </b-collapse>
+    </b-card>
 </div>
 </template>
 
@@ -29,6 +27,7 @@ import axios from 'axios'
 import usercookie from '../util/usercookie.js'
 export default {
   name: 'collections',
+  props: ['bookmarks'],
   data() {
     return {
       collections: [],
@@ -43,29 +42,64 @@ export default {
   methods: {
     getCollections() {
       const self = this
-      self.collections = [ ]
+      self.collections = []
       axios.get('/api/v1/collections/' + usercookie.getUsername())
-        .then(function(response) {
+        .then(function (response) {
           self.collections = response.data
         })
 
-        .catch(function(error) {
+        .catch(function (error) {
           self.errMsg = error.response;
           console.log(error)
         });
     },
     showBookmarks() {
       const self = this
-  self.bookmarksForCollection = []
+      self.bookmarksForCollection = []
+      const collectionID = self.collections._id
+      console.log("CollectionID: " + collectionID)
       axios.get('/api/v1/collections/' + usercookie.getUsername() + '/' + '5b30ec0465e12d00013d008b')
-        .then(function(response) {
+        .then(function (response) {
           self.bookmarksForCollection = response.data
           console.log(self.bookmarksForCollection)
+        })
+        .catch(function (error) {
+          self.errMsg = error.response;
+          console.log(error)
+        });
+    },
+
+    getBookmarkList() {
+      const self = this
+      self.collections.forEach(function (elem){
+          axios.get('/api/v1/collections/'.concat(usercookie.getUsername()).concat('/').concat(elem._id))
+          console.log(elem._id)
+            .then(function(response) {
+            self.bookmarks.push(response.data)
         })
         .catch(function(error) {
           self.errMsg = error.response;
           console.log(error)
         });
+      });
+    },
+    filterForViewURI(linksArray) {
+      if(linksArray) {
+        return linksArray.filter(elem => elem.webLinkType == 'ViewURL')[0].webLinkURI
+      }
+      return '#'
+    },
+    showDescription(description) {
+      let result = description.replace(/(<([^>]+)>)/ig, '')
+      let limit = 850
+      if (result.length > limit) result = result.substr(0,limit) + ' [...]'
+      return result
+    },
+    filterForexistingBookmarks(bookmarksArray) {
+      if(bookmarksArray) {
+        return bookmarksArray.filter(elem => elem.webLinkType == 'ViewURL')[0].webLinkURI
+      }
+      return 'hallo'
     }
   }
 }
