a7b9f90d4bdf8cf1e0062b3df27297a9bb42a704
babatech <shahabi1209@gmail.com>
2018-07-13 18:37:43 +0200

    Bookmark list as component + show bookmark button API call dynamically

--- src/components/BookmarkList.vue
+++ src/components/BookmarkList.vue
@@ -1,20 +1,32 @@
 <template>
 
 <div>
-  <b-card class="mb-3" v-for="bookmark in bookmarks" :key="bookmark._id">
-    
-    <h5>
-      <a :href='filterForViewURI(bookmark[0]._source.webLinks)'>{{ bookmark[0]._source.titles[0].value }}</a>
-    </h5>
-    <h6>{{bookmark[0]._id}}</h6>
-    <h6>{{bookmark[0]._source.publisher}}</h6>
-    {{bookmark[0]._source.publicationYear}}
-    <br>
-    <b-button-group>
-      <b-button disabled variant="link">More information</b-button>
-      <b-button disabled variant="link">Remove</b-button>
-    </b-button-group>
-  </b-card>
+  <b-btn  v-b-toggle="'bookmarks-'+collection._id" size="sm" v-on:click="getBookmarkList(collection._id)"  variant="link" class="text-uppercase">Show data sets</b-btn>
+  <b-collapse  v-bind:id="'bookmarks-'+collection._id" class="mt-2" accordion="bookmarks">
+    <b-card v-if="bookmarksForCollection !=='processing'">
+      <div v-if="bookmarksForCollection.length === 0">
+        No results
+      </div>
+      <div v-else >
+        <div class="m-2" v-for="bookmark  in bookmarksForCollection" :key="bookmark._id"  v-bind:id="'bookmark-'+bookmark._id">
+          Here should be shown a card with data set
+          <pre>
+                {{bookmark._id}}
+                {{bookmark._source.researchDisciplines}}
+              </pre>
+          <br>
+          <b-button-group>
+            <b-button disabled variant="link">More information</b-button>
+            <b-button disabled variant="link">Remove</b-button>
+          </b-button-group>
+        </div>
+      </div>
+    </b-card>
+    <b-card v-else >
+      loading
+    </b-card>
+  </b-collapse>
+
 </div>
 </template>
 
@@ -25,22 +37,42 @@ import usercookie from '../util/usercookie.js'
 import axios from 'axios'
 export default {
   name: 'bookmark-list',
-  props: ['collections'],
+  props: ['collections','collection'],
   data() {
     return {
       bookmarks: [],
-      filteredBookmarks: []
+      filteredBookmarks: [],
+      bookmarksForCollection: []
     }
   },
   created() {
     axios.defaults.timeout = 10000;
-    this.getBookmarkList()
+    //this.getBookmarkList()
 
   },
   methods: {
-    getBookmarkList() {
+    getBookmarkList: function(collectionID) {
       const self = this
-      self.collections.forEach(function (elem){
+      if (self.lastcollectionID && self.lastcollectionID === collectionID) {
+        // this block is to ignore the api call when the bookmark option is click 2nd time
+        self.lastcollectionID = null
+      } else {
+        self.lastcollectionID = collectionID
+        self.bookmarksForCollection = "processing"
+        console.log('/api/v1/collections/' + usercookie.getUsername() + '/' + collectionID)
+        axios.get('/api/v1/collections/' + usercookie.getUsername() + '/' + collectionID)
+          .then(function (response) {
+            self.bookmarksForCollection = []
+            self.bookmarksForCollection = response.data
+            console.log(response)
+          })
+          .catch(function (error) {
+            self.bookmarksForCollection = []
+            self.errMsg = error.response;
+            console.log(error)
+          });
+      }
+      /*self.collections.forEach(function (elem){
           axios.get('/api/v1/collections/'.concat(usercookie.getUsername()).concat('/').concat(elem._id))
             .then(function(response) {
             self.bookmarks.push(response.data)
@@ -49,7 +81,7 @@ export default {
           self.errMsg = error.response;
           console.log(error)
         });
-      });
+      });*/
     },
     filterForViewURI(linksArray) {
       if(linksArray) {
@@ -63,7 +95,7 @@ export default {
       if (result.length > limit) result = result.substr(0,limit) + ' [...]'
       return result
     },
-    
+
   }
 }
 </script>
--- src/components/Collections.vue
+++ src/components/Collections.vue
@@ -1,22 +1,12 @@
 <template>
 <div>
 <br>
-<b-card class="m-2" v-for="collection in collections" :key="collection.id">
+  <b-card class="m-2" v-for="(collection, ckey, cindex) in collections" v-bind:key="collection._id" v-bind:id="'collection-'+collection._id">
       <h3>{{collection.name}}</h3>
       <h5>{{collection._id}}</h5>
       <br>
-      <b-btn v-b-toggle.collapse1_inner size="sm" v-on:click="showBookmarks" variant="link" class="text-uppercase">Show data sets</b-btn>
-      <b-collapse id=collapse1_inner class="mt-2">
-        <b-card class="m-2" v-for="bookmark in bookmarksForCollection" :key="bookmark.id">   
-          Here should be shown a card with data set
-          <br>
-          <b-button-group>
-            <b-button disabled variant="link">More information</b-button>
-            <b-button disabled variant="link">Remove</b-button>
-          </b-button-group>
-        </b-card>
-      </b-collapse>
-    </b-card>
+      <bookmark-list :collection="collection"></bookmark-list>
+  </b-card>
 </div>
 </template>
 
@@ -24,6 +14,7 @@
 /* eslint-disable */
 
 import axios from 'axios'
+import BookmarkList from './BookmarkList.vue'
 import usercookie from '../util/usercookie.js'
 export default {
   name: 'collections',
@@ -53,22 +44,6 @@ export default {
           console.log(error)
         });
     },
-    showBookmarks() {
-      const self = this
-      self.bookmarksForCollection = []
-      const collectionID = self.collections._id
-      console.log("CollectionID: " + collectionID)
-      axios.get('/api/v1/collections/' + usercookie.getUsername() + '/' + '5b30ec0465e12d00013d008b')
-        .then(function (response) {
-          self.bookmarksForCollection = response.data
-          console.log(self.bookmarksForCollection)
-        })
-        .catch(function (error) {
-          self.errMsg = error.response;
-          console.log(error)
-        });
-    },
-
     getBookmarkList() {
       const self = this
       self.collections.forEach(function (elem){
