fca45a6f11239e9d5bfb3a69b681f14487bb95db
Anastasia Kazakova <a.kazakova@zbw.eu>
2019-02-11 16:05:52 +0100

    final version

--- src/components/SearchFacetes.vue
+++ src/components/SearchFacetes.vue
@@ -166,7 +166,7 @@ export default {
 
 .btn-accordion-gerdi {
   border: 0 none;
-  font-weight: normal;
+  font-weight: 700;
   letter-spacing: 0.1px;
   outline: 0 none;
   background: transparent;
